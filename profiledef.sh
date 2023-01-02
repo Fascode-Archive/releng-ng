e5e3ebd649a19937501dbbe7362e924e486cacff
hayao <hayao@fascode.net>
2023-01-02 17:56:24 +0900

    Initial commit

---
+++ profiledef.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="archlinux"
+iso_label="ARCH_$(date +%Y%m)"
+iso_publisher="Arch Linux <https://archlinux.org>"
+iso_application="Arch Linux Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
+
+aur_packages="aur_packages.${arch}"
+bootstrap_aur_packages="bootstrap_aur_packages.${arch}"
+
+aur_helper_command="yay"
+aur_helper_package="yay"
+aur_helper_depends=("go" "base-devel" "git")
+aur_helper_args=(
+    --useask
+    --mflags "-AcC"
+    --mflags "--skippgpcheck"
+    --aur
+    --nocleanmenu
+    --nodiffmenu
+    --noeditmenu
+    --noupgrademenu
+    --noprovides
+    --removemake
+)
